2cad30b5d449c0a62f152725b2a54f2c5af568bf
Bartosz <barzak200@gmail.com>
2021-10-18 16:45:32 +0200

    Try to deploy to github-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,41 +1,40 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'ChooseColorNuxt',
-    htmlAttrs: {
-      lang: 'en'
+    // https://nuxtjs.org/deployments/github-pages/
+    target: "static",
+    router: {
+        base: "/ChooseColorNuxt/"
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "ChooseColorNuxt",
+        htmlAttrs: {
+            lang: "en"
+        },
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: "" },
+            { name: "format-detection", content: "telephone=no" }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {}
+};
