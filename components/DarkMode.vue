4b94335e1ffbf985b2e7c0621159a44e64d21fc4
Bartosz <barzak200@gmail.com>
2021-10-20 20:49:35 +0200

    Style dark mode button

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,15 +1,45 @@
 <template>
-  <button @click="toggleDarkMode">Dark Mode</button>
+  <button @click="toggleDarkMode">{{ content }}</button>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      content: "🌞",
+      dark: false
+    };
+  },
   methods: {
     toggleDarkMode() {
       document.body.classList.toggle("dark-mode");
+      if (this.content == "🌙") {
+        this.content = "🌞";
+      } else {
+        this.content = "🌙";
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  background: none;
+  border: none;
+
+  font-size: 2.4rem;
+
+  position: absolute;
+  left: 3%;
+  top: 0.3rem;
+
+  cursor: pointer;
+}
+
+@media (min-width: 62em) {
+  button {
+    left: 25%;
+  }
+}
+</style>
