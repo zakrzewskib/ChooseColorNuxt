6af1f93ed8498010db871c48d63b36a8d833172b
Bartosz <barzak200@gmail.com>
2021-11-01 11:03:57 +0100

    Add screenshots

--- test/Test.spec.js
+++ test/Test.spec.js
@@ -1,18 +1,11 @@
 // Import the `mount()` method from Vue Test Utils
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import ColorBox from "~/components/ColorBox.vue";
 
-test("test", async() => {
-    const wrapper = mount(ColorBox);
+test("test", () => {
+    const color = "#111111";
+    const wrapper = shallowMount(ColorBox, {
+        propsData: { color }
+    });
     console.log(wrapper);
-
-    const div = wrapper.find("div");
-    console.log(div);
-    // const text = wrapper.find("p");
-
-    // expect(text.text()).toContain("Total clicks: 0");
-
-    // await button.trigger("click");
-
-    // expect(text.text()).toContain("Total clicks: 1");
 });
